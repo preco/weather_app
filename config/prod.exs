b8e5c4d169694b05900823ca4a6096f32a174eff
Thiago Schwartz <perereco@gmail.com>
2020-05-27 14:22:54 -0300

    Adicionado comando para carregar variaveis

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :weather_app, WeatherAppWeb.Endpoint,
   http: [port: {:system, "PORT"}],
+  load_from_system_env: true,
   url: [scheme: "https", host: "tubarao-weather.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
