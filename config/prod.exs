0357f18c3cb8bbb3538a50b679362bda45670f4f
Thiago Schwartz <perereco@gmail.com>
2020-06-15 16:43:41 -0300

    Alterada configuração de producao

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :weather_app, WeatherAppWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME")<> ".gigalixirapp.com", port: 80],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server:true
+  server: true
 
 config :logger, level: :info
 
